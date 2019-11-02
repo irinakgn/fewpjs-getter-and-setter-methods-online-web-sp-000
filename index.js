7b8fa323a31d612cbad55786b287a8fd60c29a8e
Irina Kagan <irinagamb@gmail.com>
2019-11-02 00:30:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,15 +12,11 @@ class Circle {
   get circumference() {
     return 2 * Math.PI * this.radius;
   }
-   set circumference(newCircumference){
-     this.radius = 2 * Math.PI;
-   }
+
 
   get area() {
     return Math.PI * this.radius * this.radius;
   }
 
-  set area(newArea) {
-    this.radius = Math.sqrt(newArea) * Math.PI;
-  }
+  
 }
